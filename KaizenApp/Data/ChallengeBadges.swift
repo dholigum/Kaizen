de0a13974d6b15f3a81778217eed053d2c999e08
dholigum <syahrulfadholi@mail.ugm.ac.id>
2021-05-03 21:59:44 +0700

    Level accomplishment badges done

---
+++ KaizenApp/Data/ChallengeBadges.swift
@@ -0,0 +1,10 @@
+//
+//  ChallengeBadges.swift
+//  KaizenApp
+//
+//  Created by Syahrul Apple Developer BINUS on 03/05/21.
+//
+
+import Foundation
+
+
--- KaizenApp/View/Badges.swift
+++ KaizenApp/View/Badges.swift
@@ -11,6 +11,8 @@ struct Badges: View {
     
     @ObservedObject var homeData = TaskViewModel()
     @State var currentLevel = UserDefaults.standard.integer(forKey: "level")
+    @State var showBadgesDetail = false
+    @State var levelSelected: Int = 1
     
     var levels: [Level] = levelData
     
@@ -31,7 +33,7 @@ struct Badges: View {
                     Color(.white)
                     
                     VStack(alignment: .leading) {
-                        Text("Level Accomplishment \(currentLevel)")
+                        Text("Level Accomplishment")
                             .font(.system(size: 18))
                             .fontWeight(.bold)
                             .foregroundColor(.black)
@@ -39,27 +41,108 @@ struct Badges: View {
                         ScrollView(.horizontal, showsIndicators: false, content: {
                             LazyHStack(spacing: 16) {
                                 ForEach(levels) { item in
-                                    VStack {
-                                        Text("\(item.level)")
-                                            .foregroundColor(.black)
-                                    }
-                                    .frame(width: 100, height: 100, alignment: .center)
-                                    .background(
-                                        item.level <= 3 ? LinearGradient(gradient: .init(colors: [Color("maincolor"), Color("maincolor2")]), startPoint: .top, endPoint: .bottom)
-                                        : LinearGradient(gradient: .init(colors: [Color("accentcolor")]), startPoint: .top, endPoint: .bottom)
-                                    )
-                                    .cornerRadius(12)
+                                    
+                                    Button(action: {
+                                        if item.level <= currentLevel {
+                                            withAnimation {
+                                                showBadgesDetail.toggle()
+                                                levelSelected = item.level
+                                            }
+                                        } else {
+                                            return
+                                        }
+                                    }, label: {
+                                        if item.level <= currentLevel {
+                                            VStack {
+                                                ZStack {
+                                                    Image("congrats-ribbon")
+                                                        .resizable()
+                                                        .frame(width: 50, height: 45, alignment: .center)
+
+                                                    Text("\(item.level)")
+                                                        .font(.title)
+                                                        .fontWeight(.bold)
+                                                        .foregroundColor(Color("yellowcolor"))
+                                                        .padding(.bottom, 10)
+                                                }
+                                                Text("Level \(item.level) Complete")
+                                                    .font(.system(size: 8))
+                                                    .foregroundColor(.white)
+                                            }
+                                            .frame(width: 80, height: 80, alignment: .center)
+                                            .background(
+                                                LinearGradient(gradient: .init(colors: [Color("maincolor"), Color("maincolor2")]), startPoint: .top, endPoint: .bottom)
+                                            )
+                                            .cornerRadius(12)
+                                        } else {
+                                            VStack {
+                                                
+                                                Text("Complete Level \(item.level) to unlock this badges")
+                                                    .font(.system(size: 10))
+                                                    .multilineTextAlignment(.center)
+                                                    .foregroundColor(.gray)
+                                                    .padding(.all, 4)
+                                            }
+                                            .frame(width: 80, height: 80, alignment: .center)
+                                            .background(
+                                                LinearGradient(gradient: .init(colors: [Color("accentcolor")]), startPoint: .top, endPoint: .bottom)
+                                            )
+                                            .cornerRadius(12)
+                                        }
+                                    })
+                                    
                                 }
                             }
                         })
-                        .padding(.trailing, 16)
+                    }
+                    .padding()
+                }
+                .frame(width: UIScreen.main.bounds.width - 40, height: 160)
+                .padding(.trailing, 2)
+                .cornerRadius(8)
+                
+                // Challenge Accomplishmen
+                ZStack(alignment: Alignment(horizontal: .leading, vertical: .top)) {
+                    Color(.white)
+                    
+                    VStack(alignment: .leading) {
+                        Text("Challenge Accomplishment \(currentLevel)")
+                            .font(.system(size: 18))
+                            .fontWeight(.bold)
+                            .foregroundColor(.black)
+                        
+                        ScrollView(.horizontal, showsIndicators: false, content: {
+                            LazyHStack(spacing: 16) {
+                                ForEach(levels) { item in
+                                    
+                                    Button(action: {
+                                        withAnimation {
+                                            showBadgesDetail.toggle()
+                                        }
+                                    }, label: {
+                                        VStack {
+                                            Text("\(item.level)")
+                                        }
+                                        .frame(width: 80, height: 80, alignment: .center)
+                                        .background(
+                                            item.level <= 3 ? LinearGradient(gradient: .init(colors: [Color("maincolor"), Color("maincolor2")]), startPoint: .top, endPoint: .bottom)
+                                                : LinearGradient(gradient: .init(colors: [Color("accentcolor")]), startPoint: .top, endPoint: .bottom)
+                                        )
+                                        .cornerRadius(12)
+                                    })
+                                    
+                                }
+                            }
+                        })
                     }
                     .padding()
                 }
                 .frame(width: UIScreen.main.bounds.width - 40, height: 160)
                 .cornerRadius(8)
+                
             }
             .padding()
+            
             
             Button(action: {homeData.isPresentedBadgesView.toggle()}, label: {
                 Image(systemName: "xmark")
@@ -69,6 +152,18 @@ struct Badges: View {
                     .padding(.top)
                     .padding(.top, 8)
             })
+            
+            if showBadgesDetail {
+                BadgesDetailView(show: $showBadgesDetail, levelSelected: levelSelected)
+                    .frame(width: UIScreen.main.bounds.size.width - 60, height: UIScreen.main.bounds.size.width - 120, alignment: .center)
+                    .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.25), radius: 12, x: 6, y: 6)
+                    .alignmentGuide(.trailing, computeValue: { dimension in
+                        dimension[HorizontalAlignment.trailing] + 28
+                    })
+                    .alignmentGuide(.top, computeValue: { dimension in
+                        dimension[VerticalAlignment.top] - 280
+                    })
+            }
         }
     }
 }
