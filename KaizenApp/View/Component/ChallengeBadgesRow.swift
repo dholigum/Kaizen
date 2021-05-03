2f169e59597de73759f643c4b85f44bf401593ce
dholigum <syahrulfadholi@mail.ugm.ac.id>
2021-05-04 00:43:21 +0700

    Fix minor bug badges should not act like button

--- KaizenApp/View/Component/ChallengeBadgesRow.swift
+++ KaizenApp/View/Component/ChallengeBadgesRow.swift
@@ -33,46 +33,36 @@ struct ChallengeBadgesRow: View {
         HStack(spacing: 16) {
             ForEach(challengeType) { item in
                 
-                Button(action: {
-                    if item.countToUnlock <= count {
-                        withAnimation {
-                            showBadgesDetail.toggle()
-                        }
-                    } else {
-                        return
+                if item.countToUnlock <= count {
+                    VStack {
+                        Image("\(item.badgesImage)")
+                            .resizable()
+                            .frame(width: 50, height: 45, alignment: .center)
+                        Text("\(item.difficulty) Task \(item.nickname)")
+                            .font(.system(size: 8))
+                            .foregroundColor(.white)
                     }
-                }, label: {
-                    if item.countToUnlock <= count {
-                        VStack {
-                            Image("\(item.badgesImage)")
-                                .resizable()
-                                .frame(width: 50, height: 45, alignment: .center)
-                            Text("\(item.difficulty) Task \(item.nickname)")
-                                .font(.system(size: 8))
-                                .foregroundColor(.white)
-                        }
-                        .frame(width: 85, height: 85, alignment: .center)
-                        .background(
-                            LinearGradient(gradient: .init(colors: [Color("maincolor"), Color("maincolor2")]), startPoint: .top, endPoint: .bottom)
-                                
-                        )
-                        .cornerRadius(12)
-                    } else {
-                        VStack {
-                            Text("Complete \(item.countToUnlock) \(item.difficulty) Tasks to unlock this badges")
-                                .font(.system(size: 10))
-                                .multilineTextAlignment(.center)
-                                .foregroundColor(.gray)
-                                .padding(.all, 4)
-                        }
-                        .frame(width: 85, height: 85, alignment: .center)
-                        .background(
-                            LinearGradient(gradient: .init(colors: [Color("accentcolor")]), startPoint: .top, endPoint: .bottom)
-                                
-                        )
-                        .cornerRadius(12)
+                    .frame(width: 85, height: 85, alignment: .center)
+                    .background(
+                        LinearGradient(gradient: .init(colors: [Color("maincolor"), Color("maincolor2")]), startPoint: .top, endPoint: .bottom)
+                            
+                    )
+                    .cornerRadius(12)
+                } else {
+                    VStack {
+                        Text("Complete \(item.countToUnlock) \(item.difficulty) Tasks to unlock this badges")
+                            .font(.system(size: 10))
+                            .multilineTextAlignment(.center)
+                            .foregroundColor(.gray)
+                            .padding(.all, 4)
                     }
-                })
+                    .frame(width: 85, height: 85, alignment: .center)
+                    .background(
+                        LinearGradient(gradient: .init(colors: [Color("accentcolor")]), startPoint: .top, endPoint: .bottom)
+                            
+                    )
+                    .cornerRadius(12)
+                }
             }
         }
         .padding(.bottom, 10)
